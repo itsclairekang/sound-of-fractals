d029906786abe178ad7c4db65371c68f97c5f2b3
Claire Kang <82342929+itsclairekang@users.noreply.github.com>
2023-10-10 16:44:13 -0400

    work on background image stuff

--- src/visual_render.js
+++ src/visual_render.js
@@ -1,29 +1,28 @@
 /*
 TODO: 
 1) Get the slider functionalities working
-2) Have canvas width and height be just the center section of the page
-3) have the canvas position be centered window minus image header space
 4) Get the slider positions to be correct 
-
 */
 
 window.addEventListener('load', function() {
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
+    const logoHeight = this.document.getElementById('logo').offsetHeight;
+    canvas.width = window.innerWidth * 0.8;
+    canvas.height = window.innerHeight - logoHeight;
+    canvas.style.top = logoHeight + "px";     
+    
     //canvas settings
     ctx.lineWidth = 10;
     ctx.lineCap = 'round';
     
     //effect settings
     let size = canvas.width < canvas.height ? canvas.width * 0.15 : canvas.height * 0.15;
-    let sides = 4;
-    let layers = 8;
+    let sides = 6;
+    let layers = 5;
     let scale = 0.5;
-    let spread = 0.6; // 0.1 - 2.5
-    let branches = 2;
+    let spread = 0.2; // 0.1 - 2.5
+    let branches = 4;
     let color = 'hsl(180, 80%, 70%)';
 
     //controls
